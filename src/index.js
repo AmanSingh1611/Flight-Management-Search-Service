2091bff844f35510e0e2bf277c928c3097080ef8
Aman <aman16024375@gmail.com>
2024-03-10 00:10:50 +0530

    Added few dependemcies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const {PORT}=require('./config/serverconfig');
 const setupAndStartServer = async ()=>{
     //create express object
     const app=express();
-    const PORT=3000;
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
         //console.log(process.env)
