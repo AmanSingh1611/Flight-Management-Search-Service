b4f51f1a4077151d7c530edb9ad797f2c000a12b
Aman <aman16024375@gmail.com>
2024-03-31 15:20:06 +0530

    added routes and crud of city

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-const express=require('express');
-const {PORT}=require('./config/serverconfig');
-const CityRepository=require('./repository/city-repository')
-const setupAndStartServer = async ()=>{
-    //create express object
-    const app=express();
-    app.listen(PORT,async ()=>{
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
+
+const db = require('./models/index');
+// const {Airplane} = require('./models/index');
+
+const setupAndStartServer = async () => {
+
+    // create the express object
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', ApiRoutes);
+
+    app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
-        //console.log(City);
-        //console.log(process.env)
-       
-    })
+    });
 }
 
 setupAndStartServer();
